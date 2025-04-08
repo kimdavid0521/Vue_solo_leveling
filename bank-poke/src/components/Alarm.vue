212c0973facc85437d6e32952afd6df33553514b
glorypnag <dys1128@naver.com>
2025-04-08 11:41:04 +0900

    [Feat] 로그인 아이디 전역선언 (#6)

---
+++ bank-poke/src/components/Alarm.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>알림</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const alarm = ref([]);
+
+const getAlarm = async () => {
+  const response = await fetch('/api/users/');
+  const data = await response.json();
+  alarm.value = data;
+};
+</script>
+
+<style scoped></style>
--- bank-poke/src/pages/Main.vue
+++ bank-poke/src/pages/Main.vue
@@ -1,3 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <Alarm />
+    <div>
+      <h1>메인페이지입니다</h1>
+      <p>로그인한 유저: {{ authStore.user?.id }}</p>
+    </div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth';
+import Alarm from '@/components/Alarm.vue';
+
+const authStore = useAuthStore();
+console.log('로그인한 유저 ID:', authStore.user?.id); // 콘솔 출력
+</script>
